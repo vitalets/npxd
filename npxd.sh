b17bfbe1bc4a1c4e6a01b161686a79ec463bd6d1
vitalets <vitalets@yandex-team.ru>
2020-10-30 18:08:01 +0300

    remove usage from code

--- npxd.sh
+++ npxd.sh
@@ -2,14 +2,12 @@
 # If inside container - just run command.
 # If outside container - ensure container is running and run command inside container.
 #
+# USAGE:
+# ./npxd.sh <command>
+#
 # See: https://github.com/vitalets/npxd
 
 set -e
-
-if [ $# -eq 0 ]; then
-  echo "USAGE: npxd <command> OR ./npxd.sh <command>"
-  exit 0
-fi
 
 if grep -sq docker /proc/1/cgroup; then
   npx "$@"
